53b39c619c50dd709f6540c46fd70424c13150c5
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-01-15 22:01:43 +0700

    add struct generator

---
+++ include/macrojson/object.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2026, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#ifdef MJSON_OBJECT_BEGIN
+#  undef MJSON_OBJECT_BEGIN
+#endif
+
+#ifdef MJSON_FIELD
+#  undef MJSON_FIELD
+#endif
+
+#ifdef MJSON_OBJECT_END
+#  undef MJSON_OBJECT_END
+#endif
+
+#define MJSON_OBJECT_BEGIN(obj_name) \
+    struct obj_name {
+
+#define MJSON_FIELD(type, field, ...) \
+        type field{};
+
+#define MJSON_OBJECT_END(obj_name) \
+    };
+
