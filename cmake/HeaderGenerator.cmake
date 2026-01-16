7d4c7fc2dbc74da18f7b77cc138b3805fd672831
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-01-16 20:06:23 +0700

    add macrojson header generator

--- cmake/HeaderGenerator.cmake
+++ cmake/HeaderGenerator.cmake
@@ -3,4 +3,18 @@
 #
 # License: MIT
 
-# TODO
+function(macrojson_generate_header
+    descr_file
+    output_file
+)
+    set(MJSON_HEADER_TEMPLATE
+        ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Templates/MacroJsonHeader.template)
+    set(MJSON_OUTPUT_FILE
+        ${CMAKE_CURRENT_LIST_DIR}/generated/${output_file})
+    set(MJSON_DESCR_FILE ${descr_file})
+    configure_file(
+        ${MJSON_HEADER_TEMPLATE}
+        ${MJSON_OUTPUT_FILE}
+        @ONLY
+    )
+endfunction()
