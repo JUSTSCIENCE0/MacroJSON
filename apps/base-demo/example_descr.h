2b3689958597a147a2ff0688d37864b9922fc760
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-01-16 20:07:05 +0700

    writer example draft

---
+++ apps/base-demo/example_descr.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2026, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+MJSON_OBJECT_BEGIN(SimpleExample)
+    MJSON_FIELD(int,         i32_attr)
+    MJSON_FIELD(double,      dbl_attr)
+    MJSON_FIELD(uint64_t,    u64_attr)
+    MJSON_FIELD(std::string, str_attr)
+MJSON_OBJECT_END(SimpleExample)
--- apps/base-demo/main.cpp
+++ apps/base-demo/main.cpp
@@ -3,8 +3,28 @@
 //
 // License: MIT
 
-#include <macrojson/macrojson.hpp>
+#include "example.hpp"
+
+#include <iostream>
 
 int main() {
+    SimpleExample example{
+        /* i32_attr */ 42,
+        /* dbl_attr */ 3.14,
+        /* u64_attr */ 1234567890123456789ULL,
+        /* str_attr */ "Hello, MacroJSON!"
+    };
+
+    rapidjson::Document doc;
+    doc.SetObject();
+
+    write_to_json(nullptr, example, doc);
+
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+    std::cout << "Serialized JSON:" << std::endl;
+    std::cout << buffer.GetString() << std::endl;
+
     return 0;
 }
