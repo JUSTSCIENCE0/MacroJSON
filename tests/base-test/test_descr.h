b666764764203da2b311a41c5dcf047bd576496c
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-09-21 22:35:22 +0700

    add descr for std::variant feature tests

--- tests/base-test/test_descr.h
+++ tests/base-test/test_descr.h
@@ -156,3 +156,64 @@ MJSON_POLYMORPHIC_OBJECT_BEGIN(
     MJSON_DERIVED_OBJECT_END(Object2Example)
 MJSON_POLYMORPHIC_OBJECT_END()
 #undef MJSON_BASE_OBJECT_NAME
+
+MJSON_ENUM_BEGIN(VariantTypesExample,
+        "Variant Types Example",
+        "An example enum that is used as a type discriminator for demonstrating std::variant")
+    MJSON_ENUM_UNIT(E_VARIANT_TYPE_1, variant1)
+    MJSON_ENUM_UNIT(E_VARIANT_TYPE_2, variant2)
+    MJSON_ENUM_UNIT(E_VARIANT_TYPE_3, variant3)
+    MJSON_ENUM_UNIT(E_VARIANT_INT,    variant_int)
+MJSON_ENUM_END(VariantTypesExample)
+
+MJSON_OBJECT_BEGIN(VariantType1,
+        "Variant Example Type 1",
+        "One of the options for demonstrating std::variant")
+    MJSON_FIELD(int,    v1_i32_attr,
+        "I32 Attribute",
+        "32-bit integer attribute",
+        macrojson::Range<int>{})
+    MJSON_FIELD(double, v1_dbl_attr,
+        "Double Attribute",
+        "Double-precision floating-point attribute")
+MJSON_OBJECT_END(VariantType1)
+
+MJSON_OBJECT_BEGIN(VariantType2,
+        "Variant Example Type 2",
+        "One of the options for demonstrating std::variant")
+    MJSON_FIELD(uint64_t,    v2_u64_attr,
+        "U64 Attribute",
+        "64-bit unsigned integer attribute")
+    MJSON_FIELD(std::string, v2_str_attr,
+        "String Attribute", nullptr,
+        macrojson::StringLength{ 1, 100 })
+MJSON_OBJECT_END(VariantType2)
+
+MJSON_OBJECT_BEGIN(VariantType3,
+        "Variant Example Type 3",
+        "One of the options for demonstrating std::variant")
+    MJSON_FIELD(float,         v3_flt_attr,
+        "Float Attribute",
+        "Single-precision floating-point attribute")
+    MJSON_FIELD(SimpleExample, v3_obj_attr, nullptr, nullptr)
+MJSON_OBJECT_END(VariantType3)
+
+MJSON_VARIANT_BEGIN(VariantExample, VariantTypesExample,
+        "Variant Example",
+        "An example of std::variant type")
+    MJSON_VARIANT_UNIT(VariantType1, E_VARIANT_TYPE_1)
+    MJSON_VARIANT_UNIT(VariantType2, E_VARIANT_TYPE_2)
+    MJSON_VARIANT_UNIT(VariantType3, E_VARIANT_TYPE_3)
+    MJSON_VARIANT_UNIT(int,          E_VARIANT_INT)
+MJSON_VARIANT_END(VariantExample)
+
+MJSON_OBJECT_BEGIN(VariantContainerExample,
+        "Variant Container Example",
+        "An example object that contains a std::variant attribute")
+    MJSON_FIELD(VariantExample, variant_attr,
+        "Variant Attribute",
+        "A std::variant attribute")
+    MJSON_FIELD(std::vector<VariantExample>, variant_arr,
+        "Variant Array Attribute",
+        "An array of std::variant attributes")
+MJSON_OBJECT_END(VariantContainerExample)
